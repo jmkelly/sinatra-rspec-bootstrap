e07ba99949a677439b713ebef7d7d8fbbc90e03e
James Kelly <james.kelly.99@gmail.com>
2014-04-30 20:24:13 +1000

    add database.yaml and template database info

--- application.rb
+++ application.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'sequel'
+require 'logger'
+require 'yaml'
 require "sinatra/reloader" if development?
+
+DB = Sequel.connect(YAML.load_file('config/database.yaml')[settings.environment.to_s]) 
+DB.loggers << Logger.new($sdout)
 
 get '/' do
     erb :landing, :layout => :index 
@@ -8,3 +14,9 @@ end
 get '/main' do 
     erb :main, :layout => :index
 end
+
+get '/temperatures' do
+  start_date = params[:start_date]
+  end_date = params[:end_date]
+end
+
